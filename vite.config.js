ed36b2f22c18b029bfd3c691638512acc42dd0ce
ramialmallahi <raminazmi7@gmail.com>
2025-03-18 16:50:11 +0200

    commit "30"

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://rosedye-backend-production.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
         configure: (proxy, options) => {
